2c6b1222f246362d574c534239bd8beae13d4a00
sean violante <sean.violante@zalando.de>
2018-12-28 18:10:31 +0100

    working local prediction

---
+++ vowpalwabbit/container/vowpalwabbit/predictor.py
@@ -0,0 +1,66 @@
+# This is the file that implements a flask server to do inferences. It's the file that you will modify to
+# implement the scoring for your own algorithm.
+
+from __future__ import print_function
+
+import os
+import subprocess
+import json
+import pickle
+import io
+import sys
+import signal
+import traceback
+
+import flask
+
+import pandas as pd
+
+prefix = '/opt/ml/'
+model_path = os.path.join(prefix, 'model')
+output_path = os.path.join(prefix, 'output')
+model_file = os.path.join(model_path, 'model.bin')
+data_file = os.path.join(output_path, 'data.vw')
+predictions_file = os.path.join(output_path, 'predictions.txt')
+
+# The flask app for serving predictions
+app = flask.Flask(__name__)
+
+@app.route('/ping', methods=['GET'])
+def ping():
+    """Determine if the container is working and healthy. In this sample container, we declare
+    it healthy if we can load the model successfully."""
+    health = os.path.exists(os.path.join(model_path, 'model.bin'))  # You can insert a health check here
+
+    status = 200 if health else 404
+    return flask.Response(response='\n', status=status, mimetype='application/json')
+
+@app.route('/invocations', methods=['POST'])
+def transformation():
+    """Do an inference on a single batch of data. In this sample server, we take data as CSV, convert
+    it to a pandas data frame for internal use and then convert the predictions back to CSV (which really
+    just means one prediction per line, since there's a single column.
+    """
+    data = None
+    
+    if flask.request.content_type == 'application/x-vowpalwabbit':
+        data = flask.request.data.decode('utf-8')
+        with open(data_file, 'w') as d:
+            d.write(data)
+        vw_command = ('vw -i {} --data {} --testonly --link logistic --loss_function logistic '
+                      '--predictions {}'
+                      ).format(model_file, data_file,
+                               predictions_file)
+        stderr = subprocess.check_output(vw_command, shell=True, 
+                                         timeout=60 * 20,
+                                         stderr=subprocess.STDOUT).decode('utf-8')
+        # tried stdin pipe but gunicorn complained no file no ...?
+        print(stderr)
+    else:
+        return flask.Response(response='This predictor only supports vowpalwabbit data', status=415, mimetype='text/plain')
+    
+    # get the prediction
+    with open(predictions_file) as f:
+        predictions = f.read()
+    
+    return flask.Response(response=predictions, status=200, mimetype='text/csv')
