33528ec37df461ee716d583bd33321f85ee96fd0
sean violante <sean.violante@zalando.de>
2018-12-29 16:25:56 +0100

    minor

--- vowpalwabbit/container/vowpalwabbit/predictor.py
+++ vowpalwabbit/container/vowpalwabbit/predictor.py
@@ -26,20 +26,20 @@ predictions_file = os.path.join(output_path, 'predictions.txt')
 # The flask app for serving predictions
 app = flask.Flask(__name__)
 
+
 @app.route('/ping', methods=['GET'])
 def ping():
     """Determine if the container is working and healthy. In this sample container, we declare
     it healthy if we can load the model successfully."""
     health = os.path.exists(os.path.join(model_path, 'model.bin'))  # You can insert a health check here
-
+    print('model exists')
     status = 200 if health else 404
     return flask.Response(response='\n', status=status, mimetype='application/json')
 
+
 @app.route('/invocations', methods=['POST'])
 def transformation():
-    """Do an inference on a single batch of data. In this sample server, we take data as CSV, convert
-    it to a pandas data frame for internal use and then convert the predictions back to CSV (which really
-    just means one prediction per line, since there's a single column.
+    """Do an inference on a single batch of data.
     """
     data = None
     
